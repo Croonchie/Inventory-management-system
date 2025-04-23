82ea077f1479e1b8736fe3260987d009cf78ddba
Croonchie <salazarjoshua630@gmail.com>
2025-04-22 23:37:43 -0400

    Commit 3
    Commit 3: Add toString method to Product

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ class Product {
 getTotalValue() {
     return this.price * this.quantity;
   }
+
+toString() {
+    return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+  }
+}
